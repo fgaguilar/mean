eb13dc5398527927f8d70a3e85a249a1d1223302
Fidel Gustavo Aguilar Flores <fgaguilar@yahoo.com>
2015-04-30 11:21:17 -0400

    ANB Manejo de session por username

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var multer = require('multer');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-var conectado=false;
+var sess;
 
 var UserSchema = new mongoose.Schema({
 	username: String,
@@ -52,16 +52,10 @@ app.use(multer()); // for parsing multipart/form-data
 
 
 app.use(function (req, res, next) {
-    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
-    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
     next();
 });
 
@@ -98,24 +92,28 @@ app.get('/hello', auth, function(req, res){
 
 app.post('/login', passport.authenticate('local'), function(req,res){
 	console.log("Ingreso a login");
-	console.log(req.user);
-	conectado=true;
-	console.log(conectado);
-	console.log(req.isAuthenticated());
+  sess=req.session;
+  sess.username=req.body.username;
+  console.log("sess.username " + sess.username);
 	res.json(req.user);
 });
 
 app.post('/logout', function(req,res){
 	console.log("Ingreso a logout");
+  req.session.destroy(function(err){
+    if(err){
+      console.log(err);
+    }
+  });  
 	req.logOut();
-	conectado=false;
-	console.log(conectado);
-	res.send(200);
+	res.sendStatus(200);
 });
 
 app.get('/loggedin', function(req,res){
 	console.log("Ingreso a loggedin");
-	res.send(conectado ? req.user : '0');	
+  console.log("sess.username " + sess.username);
+  console.log(req.user);
+	res.send(sess.username ? req.user : '0');	
 	//res.send(req.isAuthenticated() ? req.user : '0');	
 });
 
