943a9b3145bc0458439113e95a5ef29bbe458aec
Fidel Gustavo Aguilar Flores <fgaguilar@yahoo.com>
2015-05-14 16:49:04 -0400

    ANB cambio de localhost or face

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(multer()); // for parsing multipart/form-data
 
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
+    res.setHeader('Access-Control-Allow-Origin', 'http://face');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
